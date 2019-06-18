32c1d6366d89665b690563960ea7b28364398b6c
pasz <pavel.kowalczyk@gmail.com>
2019-06-18 11:42:55 +0200

    feat: klasy BEM i JS

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ console.log(imie);
 console.log(wiek);
 
 console.log(`Nazywam się ${imie} i mam ${wiek} lat.`);
+
+const heading = document.querySelector('.heading');
+console.log(heading);
+
+const paragraph = document.querySelectorAll('p');
+console.log(paragraph);
+console.log(paragraph[0]);
+
+const testowy = document.querySelector('.about__paragraph--js');
+console.log(testowy.innerHTML);
+testowy.innerHTML = `Nazywam się <strong>${imie}</strong> i mam ${wiek} lat.!`;
