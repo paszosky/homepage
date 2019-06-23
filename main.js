8fbbb2178c766af8e2e83a35ad31368662509325
pasz <pavel.kowalczyk@gmail.com>
2019-06-23 11:44:56 +0200

    feat: flex

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const imie = "Pasza";
-const wiek = 31;
+const wiek = 80;
 
 console.log(imie);
 console.log(wiek);
@@ -16,3 +16,48 @@ console.log(paragraph[0]);
 const testowy = document.querySelector('.about__paragraph--js');
 console.log(testowy.innerHTML);
 testowy.innerHTML = `Nazywam się <strong>${imie}</strong> i mam ${wiek} lat.!`;
+
+if (wiek < 20) {
+    console.log("Masz mniej niż 20 lat");
+} else if (wiek >= 20 && wiek <= 30) {
+    console.log("Masz więcej niż 20 ale mniej niż 30 lat");
+} else {
+    console.log("Masz więcej niż 30 lat.");
+}
+
+switch (wiek) {
+    case 20:
+        console.log("masz równo 20 lat");
+    break;
+    case 30:
+        console.log("Masz równo 30 lat");
+    break;
+    default:
+        console.log(`Masz ${wiek} lat`);
+    break;
+}
+
+const czyStary = (wiek > 70) ? "Jesteś stary" : "Nie jesteś stary";
+console.log(czyStary);
+
+function calculate(x) {
+    x = x + 3;
+    console.log(x);
+    return x*7;
+}
+
+console.log(calculate(2));
+const myCalculation = calculate(4);
+console.log(myCalculation);
+
+
+const calculateFat = (x) => {
+    x = x + 3;
+    console.log(x);
+    return x*7;
+}
+
+console.log(calculateFat(5));
+
+const calculateShort = x => (x+3)*7;
+console.log(`Najkrótsza Fat ${calculateShort(5)}`);
